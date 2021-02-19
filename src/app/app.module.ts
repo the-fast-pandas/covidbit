721261e74f225d0ed79b4d716d00d9f1441f344a
LameboiHari <60195485+LameboiHari@users.noreply.github.com>
2021-02-19 12:08:33 -0500

    Changed Admin Dashboard Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,15 +15,15 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BusinessProfileComponent } from './business-profile/business-profile.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { CaseSettingsComponent } from './admin-dashboard/case-settings/case-settings.component';
 import {OverlayModule} from '@angular/cdk/overlay';
-import { CasesService } from './admin-dashboard/cases.service';
 import { AuthInterceptor } from './auth-services/authconfig.interceptor';
-import { BusinessDashboardComponent } from './business-dashboard/business-dashboard.component';
+import { BusinessDashboardComponent } from './business-dashboard/business-dashboard.component'
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { Ng2CompleterModule } from "ng2-completer";
 import { SearchWidgetComponent } from './search-widget/search-widget.component';
 import { SearchPageComponent } from './search-page/search-page.component';
+import { MapSettingsComponent } from './admin-dashboard/adminSettings/map-settings/map-settings.component';
+import { CaseSettingsComponent } from './admin-dashboard/adminSettings/case-settings/case-settings.component'
 
 
 @NgModule({
@@ -41,6 +41,7 @@ import { SearchPageComponent } from './search-page/search-page.component';
     CaseSettingsComponent,
     SearchWidgetComponent,
     SearchPageComponent,
+    MapSettingsComponent,
   ],
   imports: [
     BrowserModule,
@@ -60,7 +61,7 @@ import { SearchPageComponent } from './search-page/search-page.component';
       useClass: AuthInterceptor,
       multi: true
     },
-    [CasesService]
+    []
   ],
   bootstrap: [AppComponent]
 })
