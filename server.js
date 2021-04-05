8745f02578668ebe0173ef9a264886042b38d7d5
Teresa Costa <tcpaixao-costa@myseneca.ca>
2021-04-05 16:54:30 -0400

    just server

--- server.js
+++ server.js
@@ -26,19 +26,12 @@ app.use(express.json());
 //
 app.use(cookieParser());
 
-//app.use(express.static(__dirname + "/dist/"));  // Directory for the 'hg build'
+app.use(express.static(__dirname + "/dist/"));  // Directory for the 'hg build'
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use('/api', routes);  // Routes for our API (this is for the frontend conection)
 
 
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-})
 
 
 // Initializes the server
