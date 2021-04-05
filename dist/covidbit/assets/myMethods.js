5da7f2862c606cb38f6e29453d4e4875bf902f2e
Teresa Costa <tcpaixao-costa@myseneca.ca>
2021-04-05 19:09:17 -0400

    deploy

---
+++ dist/covidbit/assets/myMethods.js
@@ -0,0 +1,10 @@
+
+
+
+  function formatDate(date) {
+    let mm = date.getMonth() + 1;
+    let dd = date.getDate();
+    let yyyy = date.getFullYear();
+    let del = "-";
+    return dd + del + mm + del + yyyy;
+  }
