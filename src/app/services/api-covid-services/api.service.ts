a9797334112efa6048a3afadfd179e32a1687961
Teresa Costa <tcpaixao-costa@myseneca.ca>
2021-04-05 23:49:13 -0400

    clear

--- src/app/services/api-covid-services/api.service.ts
+++ src/app/services/api-covid-services/api.service.ts
@@ -13,7 +13,7 @@ export class ApiService {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
    // 'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST,GET,OPTIONS, PUT, DELETE',
+    //'Access-Control-Allow-Methods': 'POST,GET,OPTIONS, PUT, DELETE',
     //'Access-Control-Allow-Headers': 'Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization'
   });
 
