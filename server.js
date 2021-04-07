0fbf7bbeb69b8f0e132a849ee136df689ba3012a
TeresaCristina <tcpaixao-costa@myseneca.ca>
2021-04-07 00:11:13 -0400

    try to fix cors

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const express = require('express');
 var cookieParser = require('cookie-parser');
 const app = express();
 app.all(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Requested-With");
+  res.header('Access-Control-Allow-Origin', 'https://thawing-bastion-14821.herokuapp.com/');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
