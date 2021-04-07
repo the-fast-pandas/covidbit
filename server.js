7bef13669e5fef39ba7aa763c84cb1a1a2183c5a
TeresaCristina <tcpaixao-costa@myseneca.ca>
2021-04-07 00:04:38 -0400

    try to fix cors

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var cookieParser = require('cookie-parser');
 const app = express();
 app.all(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Requested-With");
   next();
 });
 
